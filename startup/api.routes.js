ed86edab3ff496132aed9110297e3a01c2ca00f9
Christian Lisangola <christian.lisangola@gmail.com>
2020-10-26 12:20:07 +0100

    Added cors for all requests

--- startup/api.routes.js
+++ startup/api.routes.js
@@ -2,12 +2,14 @@ import router from '../router';
 import express from 'express';
 import errors from '../middlewares/errors';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const { authRouter, autreRessource } = router;
 
 export default (app, logger) => {
   app.use(morgan('combined', { stream: logger.stream })); //framawork pour des logs
   app.use(express.json());
+  app.use(cors());
   app.use(express.urlencoded({ extended: false }));
   app.use(errors(logger));
   app.use('/api/auth', authRouter); //ceci peut etre n'importe quelle ressource(postes,commentaires,etc..)
